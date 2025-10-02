05bafaa7826d5567be0871bc8d162528877e7323
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-10-02 19:32:19 +0200

    commit final ejercicio 2

--- Ejercicio2/app/Http/Controllers/Micontrolador.php
+++ Ejercicio2/app/Http/Controllers/Micontrolador.php
@@ -28,18 +28,74 @@ class Micontrolador extends Controller
     }
 
     public function contarCadena($string,$cadena){
-
+        if (strlen ($cadena) == 1) { 
+        //este comprobante se puede saltar sin problemas con un whereAlpha 'cadena', '[a-zA-Z]'
+        $arrayString = str_split($string);
+        $contador = 0;
+        foreach ($arrayString as $char) {
+            if ($char == $cadena) {
+                $contador++;
+              
+        }
     }
+        return response()->json([
+            "La letra {$cadena} aparece "=> $contador
+        ]);
+    }else{
+        return response()->json([
+            "La cadena que has introducido es demasiado larga " => $cadena
+        ]);
+    }
+}
 
     public function cuentaPalabras($string){
+        $stringLimpia = trim($string);
+        $arrayString = explode(" ", $stringLimpia);
+
+        return response()->json([
+            "En {$string} hay ". count($arrayString). ' palabras '=> $arrayString
+        ]);
 
     }
 
     public function palindromo($string){
+    /**
+     * falta el control de los acentos que no tengo muy claro si deberia hacer un when con todas las opciones,
+     * se hace un poco largo de mas porque si
+     */
+    $stringLimpia = str_replace(' ','',strtolower(trim($string)));
+    $arrayString = str_split($stringLimpia);
+    $reverseString = array_reverse($arrayString);
+    $string1 = implode('', $reverseString);
+    $string2 = implode('', $arrayString);
+    $respuesta = 'no es un palindromo';
+    if ($string1 === $string2) {
+        $respuesta = 'si es un palindromo';
+    }
+    return response()->json([
+        'resultado '=> $respuesta
+    ]);
 
     }
 
     public function anagrama($string1,$string2){
+        $respuesta = 'No son anagramas';
+        $palabra1Limpia = strtolower(trim($string1));
+        $palabra2Limpia = strtolower(trim($string2));
 
+        if (strlen($palabra1Limpia) === strlen($palabra2Limpia)) {
+            $arrayPalabra1 = str_split($palabra1Limpia);
+            $arrayPalabra2 = str_split($palabra2Limpia);
+
+            sort($arrayPalabra1); //si utilizamos sort al igualar una variable devuelve un boolean, si lo usamos aqui, cambia el array original y lo pone en orden
+            sort($arrayPalabra2);
+            if ($arrayPalabra1 === $arrayPalabra2) {
+                $respuesta = 'Si son anagramas';
+            }
+
+        }
+        return response()->json([
+            "{$string1} y {$string2}"=> $respuesta
+        ]);
     }
 }
--- Ejercicio2/routes/api.php
+++ Ejercicio2/routes/api.php
@@ -9,8 +9,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('/contarCadena/{string}/{cadena}', [Micontrolador::class, 'repeticionCadena']);
+Route::get('/contarCadena/{string}/{cadena}', [Micontrolador::class, 'contarCadena'])->whereAlpha('cadena');
 Route::get('/cesar/{string}', [Micontrolador::class, 'cesar'])->whereAlphaNumeric('string');
-Route::get('/cuentaPalabras/{string}', [Micontrolador::class,'cuentaPalabras']);
-Route::get('/palindromo/{string}', [Micontrolador::class,'palindromo']);
+Route::get('/cuentaPalabras/{string}', [Micontrolador::class,'cuentaPalabras'])->where('string', '[a-zA-Z\s]+');
+Route::get('/palindromo/{string}', [Micontrolador::class,'palindromo'])->where('string', '[a-zA-Z\s]+');;
 Route::get('/anagrama/{cadena1}/{cadena2}', [Micontrolador::class,'anagrama']);
